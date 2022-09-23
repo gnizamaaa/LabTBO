488de31f5c1864ad399dc679f0d3f34a588a6747
Rhuan Garcia <rhuangt@gmail.com>
2022-09-23 16:55:01 -0300

    Aula

--- Lab02/BST.c
+++ Lab02/BST.c
@@ -31,17 +31,67 @@ void InsereArvBin(int chave, BST *Arvore)
             Arvore->dir = CriaArvBin(chave);
     }
 }
+
+int getAlturaArvBin(BST *arvore)
+{
+    if (arvore)
+        if (arvore->esq || arvore->dir)
+        {
+            int esq = getAlturaArvBin(arvore->esq);
+            int dir = getAlturaArvBin(arvore->dir);
+            if (esq > dir)
+                return esq + 1;
+            else
+                return dir + 1;
+        }
+        else
+            return 0;
+    else
+        return -1;
+}
+
+void ImprimeNo(BST *Arvore)
+{
+    if (Arvore)
+        printf("%i ", Arvore->chave);
+}
+// Varredura
+void rec_preOrder(BST *t, void (*visit)(BST *))
+{
+    visit(t);
+    if (t->esq)
+        rec_preOrder(t->esq, visit);
+    if (t->dir)
+        rec_preOrder(t->dir, visit);
+}
+void rec_inOrder(BST *t, void (*visit)(BST *))
+{
+    if (t->esq)
+        rec_inOrder(t->esq, visit);
+    visit(t);
+    if (t->dir)
+        rec_inOrder(t->dir, visit);
+}
+void rec_postOrder(BST *t, void (*visit)(BST *))
+{
+    if (t->esq)
+        rec_postOrder(t->esq, visit);
+    if (t->dir)
+        rec_postOrder(t->dir, visit);
+    visit(t);
+}
+
 void LiberaArvBin(BST *liberada)
 {
     if (liberada)
     {
-        if (liberada->dir || liberada->esq) //Eh no no meio da arvore
+        if (liberada->dir || liberada->esq) // Eh no no meio da arvore
         {
             LiberaArvBin(liberada->dir);
             LiberaArvBin(liberada->esq);
             free(liberada);
         }
-        else //Eh folha
+        else // Eh folha
         {
             free(liberada);
         }
--- Lab02/BST.h
+++ Lab02/BST.h
@@ -8,6 +8,13 @@ typedef struct bst BST;
 
 BST *CriaArvBin(int chave);
 void InsereArvBin(int chave, BST *Arvore);
+int getAlturaArvBin(BST *Arvore);
+void ImprimeNo(BST *Arvore);
+//Varredura
+void rec_preOrder(BST *t, void (*visit)(BST*));
+void rec_inOrder(BST *t, void (*visit)(BST*));
+void rec_postOrder(BST *t, void (*visit)(BST*));
+
 void LiberaArvBin(BST *liberada);
 
 #endif
